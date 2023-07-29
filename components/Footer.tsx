11c504b58f8aaea504dc2e69e74aff9815b141a6
deep toe <rakinfrhn@gmail.com>
2023-07-29 06:47:11 +0600

    finish home page car catalogue

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { footerLinks } from "@/constants/constants";
+import { footerLinks } from "@/constants";
 
 const Footer = () => {
   return (
